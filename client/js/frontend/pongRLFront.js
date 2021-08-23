eedfcf6f9e3b0bf17a5127097a0194aaeff37945
sosuke <sosuken115@gmail.com>
2021-08-23 14:56:04 +0900

    drawobjectが色を指定できるように変更

--- client/js/frontend/pongRLFront.js
+++ client/js/frontend/pongRLFront.js
@@ -9,24 +9,25 @@ class DrawState {
   }
 
   // Given an object with coordinates and size, draw it to the canvas
-  drawObject(obj) {
+  drawObject(obj, color) {
     const width = obj.width * this.canvas.width;
     const height = obj.height * this.canvas.height;
     const x = obj.x * this.canvas.width - width / 2;
     const y = obj.y * this.canvas.height - height / 2;
-    this.ctx.fillRect(x, y, width, height);
+    this.ctx.fillStyle = color;
+    fillRoundRect(this.ctx, x, y, width, height, 10);
   }
 
   // Redraw the game based on the current state
   async draw(state) {
-    this.ctx.fillStyle = "#e5e5e6";
-    this.ctx.strokeStyle = "#e5e5e6";
+    this.ctx.fillStyle = "#628DA5";
+    this.ctx.strokeStyle = "#628DA5";
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.ctx.strokeRect(0, 0, this.canvas.width, this.canvas.height);
 
-    this.drawObject(state.ball);
-    this.drawObject(state.humanPaddle);
-    this.drawObject(state.rlPaddle);
+    this.drawObject(state.ball, "#FFFFFF");
+    this.drawObject(state.humanPaddle, "#628DA5");
+    this.drawObject(state.rlPaddle, "#628DA5");
 
     return new Promise((resolve) => {
       window.requestAnimationFrame(resolve);
@@ -60,3 +61,54 @@ function drawTest() {
 $(document).ready(() => {
   drawTest();
 });
+
+/**
+ * 角が丸い四角形のパスを作成する
+ * @param  {CanvasRenderingContext2D} ctx コンテキスト
+ * @param  {Number} x   左上隅のX座標
+ * @param  {Number} y   左上隅のY座標
+ * @param  {Number} w   幅
+ * @param  {Number} h   高さ
+ * @param  {Number} r   半径
+ */
+function createRoundRectPath(ctx, x, y, w, h, r) {
+  ctx.beginPath();
+  ctx.moveTo(x + r, y);
+  ctx.lineTo(x + w - r, y);
+  ctx.arc(x + w - r, y + r, r, Math.PI * (3 / 2), 0, false);
+  ctx.lineTo(x + w, y + h - r);
+  ctx.arc(x + w - r, y + h - r, r, 0, Math.PI * (1 / 2), false);
+  ctx.lineTo(x + r, y + h);
+  ctx.arc(x + r, y + h - r, r, Math.PI * (1 / 2), Math.PI, false);
+  ctx.lineTo(x, y + r);
+  ctx.arc(x + r, y + r, r, Math.PI, Math.PI * (3 / 2), false);
+  ctx.closePath();
+}
+
+/**
+ * 角が丸い四角形を塗りつぶす
+ * @param  {CanvasRenderingContext2D} ctx コンテキスト
+ * @param  {Number} x   左上隅のX座標
+ * @param  {Number} y   左上隅のY座標
+ * @param  {Number} w   幅
+ * @param  {Number} h   高さ
+ * @param  {Number} r   半径
+ */
+function fillRoundRect(ctx, x, y, w, h, r) {
+  createRoundRectPath(ctx, x, y, w, h, r);
+  ctx.fill();
+}
+
+/**
+ * 角が丸い四角形を描画
+ * @param  {CanvasRenderingContext2D} ctx コンテキスト
+ * @param  {Number} x   左上隅のX座標
+ * @param  {Number} y   左上隅のY座標
+ * @param  {Number} w   幅
+ * @param  {Number} h   高さ
+ * @param  {Number} r   半径
+ */
+function strokeRoundRect(ctx, x, y, w, h, r) {
+  createRoundRectPath(ctx, x, y, w, h, r);
+  ctx.stroke();
+}
