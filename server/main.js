f8c012b63910bf8b258ad4f71640b730f7203203
sosuke <sosuken115@gmail.com>
2021-09-08 21:44:47 +0900

    heroku用のポートリッスンに変更

--- server/main.js
+++ server/main.js
@@ -1,10 +1,14 @@
-const port = 3000;
 const path = require("path");
 const express = require("express");
 const app = express();
 
 app.use(express.static(path.resolve(__dirname + "/../client")));
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
 app.listen(port, () => {
-  console.log("Start server port:3000");
+  console.log(`Start server port:"${port}"`);
 });
