f1b41610bb33dff8ca0bf4e1a54135ad7a98364e
sosuke <sosuken115@gmail.com>
2021-09-27 23:28:45 +0900

    webpackでscssをビルド

--- client/webpack.common.js
+++ client/webpack.common.js
@@ -1,55 +1,59 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-    entry: {
-        'app': path.join(__dirname, 'js', 'frontend', 'main.js'),
-        'worker': path.join(__dirname, 'js', 'rl', 'worker.js'),
-    },
-    output: {
-        path: path.join(__dirname, 'public'),
-        publicPath: '/public/',
-        filename: `[name].bundle.js`,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: [
-                                [
-                                    '@babel/preset-env',
-                                    {
-                                        'modules': false,
-                                        // 'useBuiltIns': 'usage',
-                                        // 'targets': '> 0.25%, not dead',
-                                        // 'corejs': 3
-                                    }
-                                ]
-                            ],
-                            plugins: ['@babel/plugin-transform-runtime'],
-                        }
-                    }
+  entry: {
+    app: path.join(__dirname, "js", "frontend", "main.js"),
+    worker: path.join(__dirname, "js", "rl", "worker.js"),
+  },
+  output: {
+    path: path.join(__dirname, "public"),
+    publicPath: "/public/",
+    filename: `[name].bundle.js`,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                [
+                  "@babel/preset-env",
+                  {
+                    modules: false,
+                    // 'useBuiltIns': 'usage',
+                    // 'targets': '> 0.25%, not dead',
+                    // 'corejs': 3
+                  },
                 ],
+              ],
+              plugins: ["@babel/plugin-transform-runtime"],
             },
-        ]
-    },
-    resolve: {
-        alias: {
-            '@tensorflow/tfjs$':
-                path.resolve(__dirname, './js/custom_tfjs/custom_tfjs.js'),
-            '@tensorflow/tfjs-core$':
-                path.resolve(__dirname, './js/custom_tfjs/custom_tfjs_core.js'),
-        }
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-          $: 'jquery',
-          jQuery: 'jquery'
-        }),
+          },
+        ],
+      },
     ],
+  },
+  resolve: {
+    alias: {
+      "@tensorflow/tfjs$": path.resolve(
+        __dirname,
+        "./js/custom_tfjs/custom_tfjs.js"
+      ),
+      "@tensorflow/tfjs-core$": path.resolve(
+        __dirname,
+        "./js/custom_tfjs/custom_tfjs_core.js"
+      ),
+    },
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+    }),
+  ],
 };
--- client/webpack.dev.js
+++ client/webpack.dev.js
@@ -1,14 +1,40 @@
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const { merge, mergeWithRules } = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+var assert = assert || require("assert");
+
+// add scss compile rule
+common.module.rules.push({
+  test: /\.scss/,
+  use: [
+    // output to link tag
+    "style-loader",
+    // bundle css
+    {
+      loader: "css-loader",
+      options: {
+        url: true,
+        importLoaders: 2,
+        sourceMap: true,
+      },
+    },
+    // bundle sass
+    {
+      loader: "sass-loader",
+      options: {
+        sourceMap: true,
+      },
+    },
+  ],
+});
 
 module.exports = merge(common, {
-    mode: 'development',
+    mode: "development",
     optimization: {
-        usedExports: true,  // enable tree shaking
+      usedExports: true, // enable tree shaking
     },
-    plugins: [
-        new BundleAnalyzerPlugin(),
-    ],
+    plugins: [new BundleAnalyzerPlugin()],
     devtool: "eval-source-map",
-});
+  });
